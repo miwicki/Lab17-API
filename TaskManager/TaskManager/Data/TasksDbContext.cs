d662623368d08912b01ca6a91408d29027d30940
Matt <wrestler.411@comcast.net>
2018-01-17 11:27:37 -0800

    My table exists!

---
+++ TaskManager/TaskManager/Data/TasksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Data
+{
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Models.Tasks> Tasks { get; set; }
+    }
+}
---
+++ TaskManager/TaskManager/Migrations/20180117191040_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TaskManager.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
